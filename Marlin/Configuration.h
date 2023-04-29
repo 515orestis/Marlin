09c54b7dd1de6cdde7c937c7bce498a7a36dd233
515orestis <orestis.gounalakis@thinksocial.limited>
2023-04-29 13:02:01 +0300

    renames

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1223,25 +1223,25 @@
 #endif
 
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
-#define X_MIN_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define Y_MIN_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define Z_MIN_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define I_MIN_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define J_MIN_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define K_MIN_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define U_MIN_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define V_MIN_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define W_MIN_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define X_MAX_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define Y_MAX_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define Z_MAX_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define I_MAX_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define J_MAX_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define K_MAX_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define U_MAX_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define V_MAX_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define W_MAX_ENDSTOP_INVERTING false       // Set to true to invert the logic of the endstop.
-#define Z_MIN_PROBE_ENDSTOP_INVERTING false // Set to true to invert the logic of the probe.
+#define X_MIN_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define Y_MIN_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define Z_MIN_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define I_MIN_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define J_MIN_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define K_MIN_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define U_MIN_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define V_MIN_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define W_MIN_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define X_MAX_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define Y_MAX_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define Z_MAX_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define I_MAX_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define J_MAX_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define K_MAX_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define U_MAX_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define V_MAX_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define W_MAX_ENDSTOP_HIT_STATE false       // Set to true to invert the logic of the endstop.
+#define Z_MIN_PROBE_ENDSTOP_HIT_STATE false // Set to true to invert the logic of the probe.
 
 // Enable this feature if all enabled endstop pins are interrupt-capable.
 // This will remove the need to poll the interrupt pins, saving many CPU cycles.
@@ -1858,7 +1858,7 @@
 // @section extruder
 
 // #define DISABLE_E                 // Disable the extruder when not stepping
-#define DISABLE_INACTIVE_EXTRUDER // Keep only the active extruder enabled
+#define DISABLE_OTHER_EXTRUDERS // Keep only the active extruder enabled
 
 // @section motion
 
